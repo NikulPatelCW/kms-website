6116cb97cd4376bff299155af96b2d717a75f20d
NikulPatelCW <91194535+NikulPatelCW@users.noreply.github.com>
2025-11-12 19:06:49 +0530

    Add image preloading functionality to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,26 @@ import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { CustomIcon } from './core/custom-icon';
+import { ImagePreloadService } from './core/image-preload.service';
 
 function initializeIcons(iconService: CustomIcon) {
   return () => {
     iconService.registerSvgIcon('telephone', 'assets/icons/telephone.svg');
     iconService.registerSvgIcon('arrow-down', 'assets/icons/arrow-down.svg');
     iconService.registerSvgIcon('arrow-up', 'assets/icons/arrow-up.svg');
+  };
+}
+
+function preloadCriticalImages(preloadService: ImagePreloadService) {
+  return () => {
+    // Preload critical above-the-fold images
+    const criticalImages = [
+      'assets/images/logo.svg', // Header logo
+      'assets/images/hero-bg-img.png', // Hero section poster
+      'assets/images/footer-logo.svg' // Footer logo (visible on most pages)
+    ];
+    
+    preloadService.preloadImages(criticalImages);
   };
 }
 
@@ -34,6 +48,12 @@ export const appConfig: ApplicationConfig = {
       provide: APP_INITIALIZER,
       useFactory: initializeIcons,
       deps: [CustomIcon],
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: preloadCriticalImages,
+      deps: [ImagePreloadService],
       multi: true
     }
   ]
