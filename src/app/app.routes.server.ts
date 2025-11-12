16df7d1754f3e1f718e4e47afec1599568761d48
NikulPatelCW <nikul.patel@onlinepsbloans.comgit>
2025-11-12 16:06:31 +0530

    update yml & ts files

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,35 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'home',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'kms-approach',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'kms-expertise/**',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'industries',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'contact-us',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Client
   }
 ];
