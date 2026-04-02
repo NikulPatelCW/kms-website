f6e34ba3e9bdbef29b1a88cb3cb63a98d1e974e5
NikulPatelCW <nikul.patel@onlinepsbloans.comgit>
2026-04-02 09:52:24 +0530

    --update textual  change

--- src/app/components/kms-expertise/multinational-overseas-structuring/multinational-overseas-structuring.ts
+++ src/app/components/kms-expertise/multinational-overseas-structuring/multinational-overseas-structuring.ts
@@ -17,7 +17,7 @@ export class MultinationalOverseasStructuring {
       'Global business requires a carefully crafted positioning strategy that balances regulatory compliance, tax efficiency, and business goals. KMS offers comprehensive or required advisory on international structuring, offshore entities, regulatory frameworks (like FEMA, OECD guidelines), FTA, Investment Protection aspects and jurisdiction selection. We help Indian businesses expand overseas and foreign businesses establish or restructure their India operations. Our approach is rooted in commercial viability and legal soundness, making global operations simpler, safer, and compliant.',
       'For families and promoters, we also advise on cross-border wealth structuring, repatriation planning, and compliance under India’s FEMA regulations, policies and guidelines. We consider Domestic laws, double taxation treaties, reporting frameworks, and operational feasibility. From structuring international holding structures to managing transfer pricing issues, we ensure your overseas ambitions are well-supported.',
     ],
-    note: 'FEMA / RBI/ Exchange control Regulations | Global Family Office | Multinational Business Structuring',
+    note: 'FEMA / RBI / Exchange Control Regulations | Global Family Office | Multinational Business Structuring | Jurisdiction Selection',
     image: 'assets/images/expertise/multinational-img.png',
     contentBgImage: false,
     reverse: false,
